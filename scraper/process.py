11581c9b362293d57a9d83b6091d4cfb617e6dda
Keerran <kierankumar@gmail.com>
2024-05-05 17:49:24 +0100

    feat(scraper): scrape character images

--- scraper/process.py
+++ scraper/process.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import requests
+from itertools import islice
+from tqdm import tqdm
+
+
+def batched(iterable, n):
+    "Batch data into tuples of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
 
 
 def main():
@@ -14,9 +26,27 @@ def main():
 
         chars[row["type"].lower()] = chars["name"].isin(users)
 
-    files = chars["name"].apply(lambda name: f"File:{name.replace(' ', '_')}_Portrait.png")
+    files = chars["name"].apply(lambda name: f"File:{name} Portrait.png")
 
-    print(len("|".join(files)))
+    batches = ["|".join(batch) for batch in batched(files, 50)]
+    results = {}
+    for batch in tqdm(batches):
+        req = requests.get(
+            "https://onepiece.fandom.com/api.php",
+            params={
+                "action": "query",
+                "format": "json",
+                "formatversion": 2,
+                "prop": "imageinfo",
+                "iiprop": "url",
+                "titles": batch,
+            }
+        )
+
+        results |= {item["title"]: item["imageinfo"][0]["url"] for item in req.json()["query"]["pages"] if "imageinfo" in item}
+
+    files = files.map(results)
+    chars["image"] = files
 
     chars.to_csv("../full.csv", index=False)
 
